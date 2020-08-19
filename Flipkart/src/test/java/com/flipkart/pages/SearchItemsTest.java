25de0b344b752766ed3ab450c3dbc00ec1560df2
18deepak <57589232+18deepak@users.noreply.github.com>
2020-08-19 19:31:36 +0530

    first commit

---
+++ Flipkart/src/test/java/com/flipkart/pages/SearchItemsTest.java
@@ -0,0 +1,14 @@
+package com.flipkart.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SearchItemsTest {
+	WebDriver driver;
+	@Test
+	public void run() {
+		Reporter.log("search items pass",true);
+	}
+
+}
---
+++ Flipkart/src/test/java/com/flipkart/pages/SearchProductTest.java
@@ -0,0 +1,12 @@
+package com.flipkart.pages;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SearchProductTest {
+	@Test
+	public void run() {
+		Reporter.log("search product pass",true);
+	}
+
+}
